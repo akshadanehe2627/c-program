57623eed8f337df75029b612ebf8e74844fa31ef
akshadanehe2627 <118713942+akshadanehe2627@users.noreply.github.com>
2022-11-20 23:10:52 -0800

    create fac.c

--- fac.c
+++ fac.c
@@ -1,11 +1,12 @@
-#include<stdio.c>
+#include<stdio.h>
 main()
 {
-  int i,fact,n;
-  printf("\n enter n");
+  int i,fact=1,n;
+  printf("\n enter n=");
   scanf("%d",&n);
-  for(i=0;i<=n;i++)
+  for(i=0;i<n;i++)
   {
     fact=fact*i;
   }
-  printf("fact",fact);
+  printf("\n fact=%d",fact);
+}
